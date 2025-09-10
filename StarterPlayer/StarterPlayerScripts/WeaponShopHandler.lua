f3a2025d61c0f6f587d622581e3064d700a56148
aye <47103484+arges203@users.noreply.github.com>
2025-09-10 23:21:40 +0300

    Update and rename spisokcreate.lua to WeaponShopHandler.lua

--- StarterPlayer/StarterPlayerScripts/WeaponShopHandler.lua
+++ StarterPlayer/StarterPlayerScripts/WeaponShopHandler.lua
@@ -1,7 +1,28 @@
--- spisokcreate.lua (переименуйте в WeaponShopHandler.lua)
+local function createWeaponFrame(weaponName, data, parent)
+	local shopGui = parent.Parent.Parent
+	local template = shopGui.MainFrame.WeaponList.WeaponTemplate
+
+	local frame = template:Clone()
+	frame.Name = weaponName
+	frame.Visible = true
+	frame.Parent = parent
+
+	-- Заполняем данные
+	frame.WeaponName.Text = data.Name
+	frame.BuyButton.Text = data.Price == 0 and "FREE" or "BUY " .. data.Price
+
+	-- СРАЗУ подключаем кнопку при создании
+	frame.BuyButton.MouseButton1Click:Connect(function()
+		print("Покупка:", weaponName)
+		game.ReplicatedStorage.EquipWeapon:FireServer(weaponName)
+		shopGui:Destroy() -- закрываем магазин
+	end)
+
+	return frame
+end
 local Players = game:GetService("Players")
 local remoteEvent = game.ReplicatedStorage.OpenWeaponShop
-local equipEvent = game.ReplicatedStorage.EquipWeapon  -- ДОБАВЬТЕ!
+local equipEvent = game.ReplicatedStorage.EquipWeapon 
 local gui = game.ReplicatedStorage.WeaponShopGUI
 local weaponData = require(game.ReplicatedStorage.WeaponData)
 local player = Players.LocalPlayer
